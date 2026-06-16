c5a26990b5dee488201faf75f445dbc59a5f7abe
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-16 15:19:49 +0200

    feat: Interpreted string VS raw string.

--- main.go
+++ main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Gophers!")
+
+	fmt.Println("\nInterpreted string:\n - This will be at the new line.\n")
+	fmt.Println(`Raw string:\n - This will NOT be at the new line, "\n" is not interpreted.`)
 }
