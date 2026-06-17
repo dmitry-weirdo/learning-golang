000f4ea9e3a1512ac2f1e32cfbffe041df23a4d5
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-17 17:47:37 +0200

    feat: Basic slices operations.

---
+++ slices.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	var s []int    // slices of ints, no size specified
+	fmt.Println(s) // [] (nil)
+
+	s = []int{1, 2, 3}
+	fmt.Println(s)
+
+	fmt.Println(s[1])
+	s[1] = 99
+	fmt.Println(s)
+
+	s2 := append(s, 5, 10, 15)
+	fmt.Println(s)
+	fmt.Println(s2)
+
+	s[0] = 666
+	fmt.Println(s)
+	fmt.Println(s2) // is NOT updated with s
+
+	s2 = slices.Delete(s2, 1, 3) // will remove elements with indices [1; 3) from slice
+	fmt.Println(s)
+	fmt.Println(s2)
+}
