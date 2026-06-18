d18825d8fb780cd1ffa04a2582b0648f2e5c9e72
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-18 16:59:26 +0200

    feat: Pseudo-code example of deferred functions usage on closing the resources.

--- deferred.go
+++ deferred.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("main 1")
@@ -12,4 +15,14 @@ func main() {
 
 	// deferred functions are LIFO, so "deferred 2" will be executed before "deferred 1"
 	defer fmt.Println("deferred 2")
+}
+
+func dbExecute() {
+	// we define close near the resource opening
+	// then, since defer functions are LIFO, first `rows` and then `db` will be closed.
+	db, _ := sql.Open("driverName", "connection string")
+	defer db.Close() // can itself produce an error
+
+	rows, _ := db.Query("some sql query")
+	defer rows.Close() // can itself produce an error
 }
