d1fe66b98100d39ff96ec6625e9537abe6904b68
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-18 17:27:28 +0200

    feat: Panic and recover in case of auto-generated panic when dividing by zero.

--- panicAndRecover.go
+++ panicAndRecover.go
@@ -10,6 +10,17 @@ func main() {
 	func1()
 
 	fmt.Println("main 2")
+
+	dividend, divisor := 10, 5
+	divideResult := divide(dividend, divisor)
+	fmt.Printf("%v divided by %v is %v \n", dividend, divisor, divideResult)
+
+	// panic: runtime error: integer divide by zero
+	dividend, divisor = 10, 0
+	divideResult = divide(dividend, divisor)
+
+	// todo: this will still called and printed if we recovered from an error in `divide`
+	fmt.Printf("%v divided by %v is %v \n", dividend, divisor, divideResult)
 }
 
 func func1() {
@@ -22,4 +33,18 @@ func func1() {
 	fmt.Println("func1 1")
 	panic("panic from func1")
 	fmt.Println("func1 2")
+}
+
+func divide(dividend, divisor int) int {
+
+	// anonymous function
+	defer func() { // will be called also on a successful function completion
+		message := recover() // nil if no panic was raised
+
+		if message != nil {
+			fmt.Printf("Recovered from: %v \n", message)
+		}
+	}()
+
+	return dividend / divisor
 }
