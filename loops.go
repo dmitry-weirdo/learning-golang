fa1a7c4d06de8472451692140b5c8512f3bb21cd
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-18 15:35:23 +0200

    feat: Three forms of the `for` loop.

---
+++ loops.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	//infiniteLoop()
+	loopTillCondition()
+	counterBasedLoop()
+}
+
+func infiniteLoop() {
+	fmt.Println("======= infiniteLoop ======= ")
+
+	i := 1
+
+	for { // infinite loop - no condition
+		fmt.Println(i)
+		i++
+	}
+}
+
+func loopTillCondition() {
+	fmt.Println("======= loopTillCondition ======= ")
+
+	i := 1
+
+	for i < 3 { // parentheses not mandatory
+		fmt.Println(i)
+		i++
+	}
+
+	fmt.Println("Done!")
+}
+
+func counterBasedLoop() {
+	fmt.Println("======= counterBasedLoop ======= ")
+
+	for i := 1; i < 4; i++ { // parentheses not mandatory
+		fmt.Println(i)
+	}
+
+	fmt.Println("Done!")
+}
