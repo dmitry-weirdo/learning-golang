85c9afe26fad93cae3887ae9904da723fd64b815
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-18 18:27:38 +0200

    refactor: Refactor the menu app a bit, extract a couple of functions and global variables.

--- structCli.go
+++ structCli.go
@@ -7,30 +7,36 @@ import (
 	"strings"
 )
 
-func main() {
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
 
-	menu := []menuItem{
-		{
-			name: "Coffee",
-			prices: map[string]float64{
-				"small":  1.65,
-				"medium": 1.80,
-				"large":  1.95,
-			},
+// we cannot use the := syntax for non-local variables
+var menu = []menuItem{
+	{
+		name: "Coffee",
+		prices: map[string]float64{
+			"small":  1.65,
+			"medium": 1.80,
+			"large":  1.95,
 		},
-		{
-			name: "Espresso",
-			prices: map[string]float64{
-				"single": 1.90,
-				"double": 2.25,
-				"triple": 2.55,
-			},
+	},
+	{
+		name: "Espresso",
+		prices: map[string]float64{
+			"single": 1.90,
+			"double": 2.25,
+			"triple": 2.55,
 		},
-	}
+	},
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+const DELIMITER = '\n'
+
+func main() {
 
 loop: // label
 	for { // infinite loop of actions
@@ -41,37 +47,16 @@ loop: // label
 		fmt.Println("2) Add item")
 		fmt.Println("q) Quit")
 
-		in := bufio.NewReader(os.Stdin)
-
-		const delimiter = '\n'
-		choice, _ := in.ReadString(delimiter)
+		choice, _ := in.ReadString(DELIMITER)
 		choice = strings.TrimSpace(choice) // required to trim the \n delimiter that will be read
 
 		// see https://www.reddit.com/r/golang/comments/1oktsft/the_indentation_of_switch_statements_really/
 		switch choice {
 		case "1": // super-ugly formatting by gofmt, no indentation within the "switch"
-			for _, item := range menu { // ignore the array index
-				fmt.Println(item.name)
-				fmt.Println(strings.Repeat("-", 10))
-
-				// iterate the prices
-				for size, price := range item.prices {
-					fmt.Printf("\t%10s: %10.2f\n", size, price) // %10 is "fill with spaces up to 10 characters"
-				}
-			}
+			printMenu()
 
 		case "2":
-			fmt.Println("Please enter the name of the new item")
-
-			name, _ := in.ReadString(delimiter)
-			name = strings.TrimSpace(name) // required to trim the \n delimiter that will be read
-
-			newItem := menuItem{
-				name:   name,
-				prices: make(map[string]float64), // you can also provide size to make
-			}
-
-			menu = append(menu, newItem)
+			addItem()
 
 		case "q":
 			// break // this will just break from `switch`, not the outer `for`
@@ -83,4 +68,30 @@ loop: // label
 
 		//fmt.Println(menu)
 	}
+}
+
+func printMenu() {
+	for _, item := range menu { // ignore the array index
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+
+		// iterate the prices
+		for size, price := range item.prices {
+			fmt.Printf("\t%10s: %10.2f\n", size, price) // %10 is "fill with spaces up to 10 characters"
+		}
+	}
+}
+
+func addItem() {
+	fmt.Println("Please enter the name of the new item")
+
+	name, _ := in.ReadString(DELIMITER)
+	name = strings.TrimSpace(name) // required to trim the \n delimiter that will be read
+
+	newItem := menuItem{
+		name:   name,
+		prices: make(map[string]float64), // you can also provide size to make
+	}
+
+	menu = append(menu, newItem)
 }
