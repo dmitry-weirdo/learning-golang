cd9610217d01352c6735c6f91a20897512c10f9b
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-19 12:20:47 +0200

    feat: Method receivers can be value and pointer receivers.

--- methods.go
+++ methods.go
@@ -7,6 +7,16 @@ func main() {
 	even := i.isEven()
 
 	fmt.Printf("MyInt %v is even: %v \n", i, even)
+
+	u := user{
+		id:       666,
+		username: "Initial name",
+	}
+
+	fmt.Printf("User before change: %v \n", u.String())
+
+	u.UpdateName("New name")
+	fmt.Printf("User after change: %v \n", u.String())
 }
 
 // method receiver cannot be a built-in type like int
@@ -17,4 +27,19 @@ type myInt int
 
 func (i myInt) isEven() bool {
 	return i%2 == 0 // super-ugly standard formatting, no space around arithmetic operators
+}
+
+type user struct {
+	id       int
+	username string
+}
+
+// value receiver
+func (u user) String() string {
+	return fmt.Sprintf("[User ID: %v, User Name: %v]", u.id, u.username)
+}
+
+// pointer receiver
+func (u *user) UpdateName(name string) {
+	u.username = name
 }
