05a245433d3c30636d789831891dbd6db043b194
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-06-19 14:51:26 +0200

    feat: Generic function with a type constraint on its generic type, so that the `+=` operator can be used.

--- generics.go
+++ generics.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	cloneSlice()
 	cloneMapExample()
+	customTypeConstraints()
 }
 
 func cloneSlice() {
@@ -55,6 +56,35 @@ func cloneMap[K comparable, V any](m map[K]V) map[K]V {
 
 	for k, v := range m {
 		result[k] = v
+	}
+
+	return result
+}
+
+func customTypeConstraints() {
+	a1 := []int{1, 2, 3}
+	a2 := []float64{3.14, 6.02}
+	a3 := []string{"foo", "bar", "baz"}
+
+	s1 := add(a1)
+	fmt.Printf("Sum of %v: %v \n", a1, s1)
+
+	s2 := add(a2)
+	fmt.Printf("Sum of %v: %v \n", a2, s2)
+
+	s3 := add(a3)
+	fmt.Printf("Sum of %v: %v \n", a3, s3)
+}
+
+type addable interface {
+	int | float32 | float64 | string // can't add bool here
+}
+
+func add[V addable](s []V) V {
+	var result V
+
+	for _, v := range s {
+		result += v // += operator is not defined on `any`
 	}
 
 	return result
