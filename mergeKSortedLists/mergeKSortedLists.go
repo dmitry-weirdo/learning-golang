34d453a84876fe4af5ddfbcd4883f3102d8461a0
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-07 14:36:12 +0200

    feat: Solution of "23. Merge k Sorted Lists". Using an implemented PQ min-heap with `*ListNode` elements.

--- mergeKSortedLists/mergeKSortedLists.go
+++ mergeKSortedLists/mergeKSortedLists.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-type ListNode struct {
+type ListNode struct { // this is provided by LeetCode precompiled classes -> do not copy it into the solution
 	Val  int
 	Next *ListNode
 }
@@ -13,8 +13,8 @@ type ListNode struct {
 // we implement heap.Interface that extends sort.Interface with Push() and Pop() methods
 
 type PriorityQueue struct {
-	items []ListNode               // todo: maybe we need pointers
-	less  func(a, b ListNode) bool // comparator function, returns boolean, not integer!
+	items []*ListNode
+	less  func(a, b *ListNode) bool // comparator function, returns boolean, not integer!
 }
 
 // implementation of sort.Interface
@@ -34,7 +34,7 @@ func (pq *PriorityQueue) Swap(i, j int) {
 
 // implementation of heap.Interface
 func (pq *PriorityQueue) Push(x any) { // interface needs `x any`, else the override will not work
-	pq.items = append(pq.items, x.(ListNode))
+	pq.items = append(pq.items, x.(*ListNode))
 }
 
 // implementation of heap.Interface
@@ -48,13 +48,45 @@ func (pq *PriorityQueue) Pop() any { // interface needs `x any`, else the overri
 }
 
 func mergeKLists(lists []*ListNode) *ListNode {
-	// todo: implement method
-	return nil
+	// k: = len(lists)
+	dummyHead := &ListNode{Val: -666, Next: nil}
+	currentNode := dummyHead
+
+	pq := &PriorityQueue{
+		less: func(a, b *ListNode) bool {
+			// comparator comparing the values of the nodes, for min-heap
+			return a.Val < b.Val
+		},
+	}
+
+	// todo: we can use array and heapify to make this faster than O(k * log k)
+	// append heads of all the lists
+	for _, head := range lists {
+		if head != nil {
+			heap.Push(pq, head)
+		}
+	}
+
+	for pq.Len() > 0 {
+		// append the top to the result list
+		smallestNode := heap.Pop(pq).(*ListNode)
+
+		currentNode.Next = smallestNode
+		currentNode = currentNode.Next
+
+		// add smallestNode.Next to the heap, if it exists
+		if smallestNode.Next != nil {
+			heap.Push(pq, smallestNode.Next)
+		}
+	}
+
+	// skip dummy head
+	return dummyHead.Next
 }
 
 func testPriorityQueue() {
 	pq := &PriorityQueue{
-		less: func(a, b ListNode) bool {
+		less: func(a, b *ListNode) bool {
 			// comparator comparing the values of the nodes, for min-heap
 			return a.Val < b.Val
 		},
@@ -73,6 +105,66 @@ func testPriorityQueue() {
 	}
 }
 
+func printList(head *ListNode) {
+	var node = head
+
+	for node != nil {
+		fmt.Printf("%d ", node.Val)
+
+		node = node.Next
+	}
+
+	fmt.Println()
+}
+
+func arrayToList(arr []int) *ListNode {
+	var node *ListNode = nil
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		nextNode := &ListNode{arr[i], node}
+
+		node = nextNode
+	}
+	return node
+}
+
+func arraysToLists(arrays ...[]int) []*ListNode {
+	lists := make([]*ListNode, len(arrays))
+
+	for i, arr := range arrays {
+		list := arrayToList(arr)
+		lists[i] = list
+	}
+
+	return lists
+}
+
+func test(arrays ...[]int) {
+	fmt.Println()
+	fmt.Println("===================")
+
+	lists := arraysToLists(arrays...) // note the ... trick, without it arrays will have type [][]int
+
+	fmt.Println("Initial lists:")
+	for _, list := range lists {
+		printList(list)
+	}
+
+	mergedList := mergeKLists(lists)
+
+	fmt.Println("Merged list: ")
+	printList(mergedList)
+}
+
+func test1() {
+	a1 := []int{1, 4, 5}
+	a2 := []int{1, 3, 4}
+	a3 := []int{2, 6}
+
+	test(a1, a2, a3)
+}
+
 func main() {
-	testPriorityQueue()
+	// testPriorityQueue()
+	test1()
 }
