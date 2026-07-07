cfe4eea56e186a71d906b27416b198008279ff31
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-07 20:21:50 +0200

    feat: Add an option where we store not the char frequency but its last index in the string. Then we can jump `left` directly to `lastIndex + 1` on duplicate.

--- longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.go
+++ longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.go
@@ -9,6 +9,64 @@ func lengthOfLongestSubstring(s string) int {
 
 	maxLength := 1
 
+	// character to its last index
+	m := make(map[byte]int)
+
+	left := 0
+	right := 0
+
+	for right < len(s) {
+		fmt.Println()
+		fmt.Printf("Left: %v, right: %v. Substring [%v; %v] = \"%v\". \n", left, right, left, right, s[left:right+1])
+
+		chRight := s[right]
+
+		if v, ok := m[chRight]; ok {
+			if v < left { // old index was before the current range -> update it to right
+				fmt.Printf("Character '%c' was before the current range [%v; %v]. Updating its lastIndex to right = %v.\n", chRight, left, right, right)
+				m[chRight] = right
+			}
+		} else { // value doesn't yet exist -> add its index
+			m[chRight] = right
+		}
+
+		if m[chRight] == right {
+			// no duplicate chars -> check whether current string is longer then max
+			currentLength := right - left + 1
+
+			if currentLength > maxLength {
+				maxLength = currentLength
+
+				fmt.Printf("Non-repeating string [%v, %v] = \"%v\" of is a new max length %v. \n", left, right, s[left:right+1], currentLength)
+			}
+
+			right++
+		} else {
+			fmt.Printf("Found repeating character '%c' at right of [%v, %v] = \"%v\". Moving left from [%v] to last index of '%c' + 1 = [%v]. \n", chRight, left, right, s[left:right+1], left, chRight, m[chRight]+1)
+
+			// duplicate char added -> move left after the lastIndex of this char
+			left = m[chRight] + 1
+
+			fmt.Printf("Moved left to [%v]. Current non-repeating substring: [%v, %v] = \"%v\". \n", left, left, right, s[left:right+1])
+
+			// last index of chRight is now right
+			m[chRight] = right
+
+			// we still move to the next position after jumping left
+			right++
+		}
+	}
+
+	return maxLength
+}
+
+func lengthOfLongestSubstringWithFrequencyMap(s string) int {
+	if len(s) < 2 { // empty string or string of length 1 are themselves non-repeating
+		return len(s)
+	}
+
+	maxLength := 1
+
 	// character to its frequency
 	m := make(map[byte]int)
 
@@ -93,7 +151,7 @@ func test3() {
 }
 
 func main() {
-	//test1()
-	//test2()
+	test1()
+	test2()
 	test3()
 }
