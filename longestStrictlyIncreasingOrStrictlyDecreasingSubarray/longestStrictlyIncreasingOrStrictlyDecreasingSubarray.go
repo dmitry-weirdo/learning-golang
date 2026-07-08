21611db1cb4c17c37d0f3c6321fc7c77c4954699
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-09 00:36:23 +0200

    feat: First version of solution of "3105. Longest Strictly Increasing or Strictly Decreasing Subarray".

---
+++ longestStrictlyIncreasingOrStrictlyDecreasingSubarray/longestStrictlyIncreasingOrStrictlyDecreasingSubarray.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+func longestMonotonicSubarray(nums []int) int {
+	n := len(nums)
+
+	// find max increasing length
+	maxIncreasingLength := 1
+
+	start := 0
+	i := 1
+
+	for i < n {
+		for (i < n) && (nums[i] > nums[i-1]) {
+			i++
+		}
+
+		newLength := i - start // i is already an element that broke the sequence or is out of an array
+
+		maxIncreasingLength = max(newLength, maxIncreasingLength)
+
+		// reset increasing sequence start
+		start = i
+		i++
+	}
+
+	fmt.Printf("Max increasing subarray length: %d \n", maxIncreasingLength)
+
+	// find max decreasing length
+	maxDecreasingLength := 1
+
+	start = 0
+	i = 1
+
+	for i < n {
+		for (i < n) && (nums[i] < nums[i-1]) {
+			i++
+		}
+
+		newLength := i - start // i is already an element that broke the sequence or is out of an array
+
+		maxDecreasingLength = max(newLength, maxDecreasingLength)
+
+		// reset increasing sequence start
+		start = i
+		i++
+	}
+
+	fmt.Printf("Max decreasing subarray length: %d \n", maxIncreasingLength)
+
+	return max(maxIncreasingLength, maxDecreasingLength)
+}
+
+func test(a []int, expectedResult int) {
+	fmt.Println()
+	fmt.Println("========================")
+
+	fmt.Printf("Array 1: %v \n", a)
+
+	result := longestMonotonicSubarray(a)
+
+	fmt.Printf("Max monotonic subarray length: %v \n", result)
+	fmt.Printf("Expected max monotonic subarray length: %v \n", expectedResult)
+
+	if result != expectedResult {
+		fmt.Printf("FAILURE: expected result = %v, actual result = %v \n", expectedResult, result)
+	}
+}
+
+func test1() {
+	arr := []int{1, 4, 3, 3, 2}
+	expected := 2
+
+	test(arr, expected)
+}
+
+func test2() {
+	arr := []int{3, 3, 3, 3}
+	expected := 1
+
+	test(arr, expected)
+}
+
+func main() {
+	// 3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+	test1()
+	test2()
+}
