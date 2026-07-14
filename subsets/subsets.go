b0b88c97962c458d032f6ef54fdd935be7806c74
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-15 01:23:59 +0200

    feat: No need to pass the array path as a pointer, updating it will also work when we pass an array.

--- subsets/subsets.go
+++ subsets/subsets.go
@@ -9,7 +9,8 @@ func subsets(nums []int) [][]int {
 
 	path := make([]int, 0)
 
-	dfs(nums, 0, &path)
+	//dfs(nums, 0, &path)
+	dfs2(nums, 0, path)
 
 	return resultGlobal
 }
@@ -41,6 +42,35 @@ func dfs(nums []int, i int, path *[]int) {
 
 	// remove the current element
 	*path = (*path)[0 : len(*path)-1]
+}
+
+func dfs2(nums []int, i int, path []int) { // no need to pass path as a pointer, editing it in place will already work
+	fmt.Println()
+	fmt.Println("==============================")
+	fmt.Printf("i: %v, path: %v \n", i, path)
+
+	if i >= len(nums) {
+		// reached the end of the array -> add the current to the result
+		pathCopy := make([]int, len(path))
+		copy(pathCopy, path)
+
+		fmt.Printf("End of the array reached. Adding current path = %v to the result. \n", pathCopy)
+
+		resultGlobal = append(resultGlobal, pathCopy)
+		fmt.Printf("Result after adding: %v. \n", resultGlobal)
+
+		return
+	}
+
+	// don't take the current element
+	dfs2(nums, i+1, path)
+
+	// take the current element
+	path = append(path, nums[i])
+	dfs2(nums, i+1, path)
+
+	// remove the current element
+	path = path[0 : len(path)-1]
 }
 
 func test(nums []int) {
