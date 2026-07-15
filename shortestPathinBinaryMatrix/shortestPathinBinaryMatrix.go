beafe7437cd4ea1d638ec9223605a516e8c5dcae
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-16 01:24:42 +0200

    feat: Since we're just doing forward-only BFS, we can use the original matrix as the `visited` indicator - setting the visited nodes to 1.

--- shortestPathinBinaryMatrix/shortestPathinBinaryMatrix.go
+++ shortestPathinBinaryMatrix/shortestPathinBinaryMatrix.go
@@ -38,6 +38,69 @@ func shortestPathBinaryMatrix(grid [][]int) int {
 		return -1
 	}
 
+	// since it's forward-only BFS without back-tracking, we can set 1 to the original matrix instead of a separate matrix
+	//visited := createVisitedMatrix(rows, columns)
+
+	queue := list.New()
+
+	// we're pushing row, col
+	// start with top-left
+	queue.PushBack([2]int{0, 0})
+	//visited[0][0] = true
+	grid[0][0] = 1
+
+	level := 1
+
+	for queue.Len() > 0 {
+		currentQueueLength := queue.Len()
+
+		for i := 0; i < currentQueueLength; i++ {
+			// pop from the queue
+			cell := queue.Remove(queue.Front()).([2]int)
+
+			row, column := cell[0], cell[1]
+
+			if (row == rows-1) && (column == columns-1) {
+				// bottom-right reached
+				return level
+			}
+
+			for _, d := range directions {
+				newRow := row + d[0]
+				newColumn := column + d[1]
+
+				if !cellExists(rows, columns, newRow, newColumn) ||
+					//visited[newRow][newColumn] || // this will be covered by setting grid[i][j] to 1 for the visited rows
+					grid[newRow][newColumn] == 1 { // we can travel only via 0 values
+					continue
+				}
+
+				grid[newRow][newColumn] = 1 // we're blocking the visited rows in the grid itself
+				queue.PushBack([2]int{newRow, newColumn})
+			}
+		}
+
+		level++
+	}
+
+	// no path found with full BFS
+	return -1
+}
+
+func shortestPathBinaryMatrixWithVisited(grid [][]int) int {
+	rows := len(grid)
+	columns := len(grid[0])
+
+	if grid[0][0] == 1 { // cannot start
+		fmt.Printf("Invalid grid: top-left is 1. Returning -1. \n")
+		return -1
+	}
+
+	if grid[rows-1][columns-1] == 1 { // cannot end
+		fmt.Printf("Invalid grid: bottom-right is 1. Returning -1. \n")
+		return -1
+	}
+
 	visited := createVisitedMatrix(rows, columns)
 
 	queue := list.New()
@@ -159,5 +222,5 @@ func test2() {
 func main() {
 	// 1091. Shortest Path in Binary Matrix
 	test1()
-	//test2()
+	test2()
 }
