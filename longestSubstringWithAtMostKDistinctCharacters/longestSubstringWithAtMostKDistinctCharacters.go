a5ef926257fc5a783f9565f8d09c59492d2b4092
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-25 01:03:49 +0200

    feat: Optimized solution of "340. Longest Substring with At Most K Distinct Characters". In case of invalid window, shrink from left just by 1. Clarifying comments and logs. - So that the window will decrease from failed `maxWindowSize + 1` attempt to `maxWindowSize`.

--- longestSubstringWithAtMostKDistinctCharacters/longestSubstringWithAtMostKDistinctCharacters.go
+++ longestSubstringWithAtMostKDistinctCharacters/longestSubstringWithAtMostKDistinctCharacters.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func lengthOfLongestSubstringKDistinct(s string, k int) int {
-	return slidingWindowShrinking(s, k)
+	return slidingWindowNonShrinking(s, k)
+	//return slidingWindowShrinking(s, k)
 }
 
 func slidingWindowShrinking(s string, k int) int {
@@ -11,7 +12,7 @@ func slidingWindowShrinking(s string, k int) int {
 	// my obvious solution, left is shrinking to become valid even if the window becomes smaller then maxWindowSize
 	n := len(s)
 	if k >= n {
-		// there are more allowed characters then the length of the string -> the complete string will fit
+		// there are more allowed characters than the length of the string -> the complete string will fit
 		return n
 	}
 
@@ -44,6 +45,63 @@ func slidingWindowShrinking(s string, k int) int {
 
 				left++
 			}
+		}
+	}
+
+	return maxWindowSize
+}
+
+func slidingWindowNonShrinking(s string, k int) int {
+	// O(n)
+	// slightly optimized -> when a window is invalid, we only shrink it from left by 1 -> to the current maxWindowSize
+	n := len(s)
+	if k >= n {
+		// there are more allowed characters than the length of the string -> the complete string will fit
+		return n
+	}
+
+	// window validity is freqMap.size <= k
+	freq := make(map[byte]int)
+
+	// string of size K cannot contain more than K distinct characters
+	maxWindowSize := k
+
+	left := 0
+
+	for right := 0; right < n; right++ {
+		fmt.Println()
+		fmt.Printf("left: %v, right: %v, windowSize: %v, maxWindowSize: %v \n", left, right, right-left+1, maxWindowSize)
+		fmt.Printf("Current window: \"%v\" \n", s[left:right+1])
+
+		// add right character to the map
+		rightChar := s[right]
+		freq[rightChar]++
+
+		if len(freq) <= k {
+			fmt.Printf("Window is valid. \n")
+
+			windowSize := right - left + 1
+			maxWindowSize = max(maxWindowSize, windowSize)
+		} else {
+			fmt.Printf("Window is invalid. Shrinking 1 from the left. \n")
+
+			// Shrink the window from left just by 1 character
+			// (it will decrease from maxWindowSize + 1 (failed attempt) to maxWindowSize),
+			// so that we keep the window on the current size (which would be maxWindowSize).
+			// The window can remain invalid, but it will be of size maxWindowSize.
+			// So we'll try to continue right-expansion into (maxWindowSize + 1) until it's valid, or we reach the end of the string.
+
+			//for len(freq) > k {
+			leftChar := s[left]
+
+			freq[leftChar]--
+
+			if freq[leftChar] <= 0 {
+				delete(freq, leftChar)
+			}
+
+			left++
+			//}
 		}
 	}
 
