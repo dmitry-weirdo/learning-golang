f24c9f14c74c1be9af2c4841a0db59a91832b1c5
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-25 00:08:52 +0200

    feat: Prefix sums solution of "209. Minimum Size Subarray Sum". O(n * log n), since we're doing binary search on prefixSums (they're guaranteed to be increasing).

--- minimumSizeSubarraySum/minimumSizeSubarraySum.go
+++ minimumSizeSubarraySum/minimumSizeSubarraySum.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func minSubArrayLen(target int, nums []int) int {
-	return slidingWindow(target, nums)
+	return calculateWithPrefixSums(target, nums)
+	//return slidingWindow(target, nums)
 }
 
 func slidingWindow(target int, nums []int) int {
@@ -34,6 +35,77 @@ func slidingWindow(target int, nums []int) int {
 	}
 
 	return minWindowSize
+}
+
+func calculateWithPrefixSums(target int, nums []int) int {
+	// this is O(n * log n) solution:
+	// O(n) - calculate prefix sums
+	// For every i in [0; n- 1], we execute the binary search that is O(log n)
+	// -> O(n * log n)
+
+	n := len(nums)
+
+	// calculate prefixSums[0; n]
+	prefixSums := make([]int, n+1)
+
+	prefixSums[0] = 0
+
+	for i, v := range nums {
+		prefixSums[i+1] = prefixSums[i] + v
+	}
+
+	fmt.Printf("Prefix sums: %v \n", prefixSums)
+
+	// !!! since all values in arr are positive, we know that prefixSums is strictly increasing,
+	// i.e. sorted, and we can binary-search in it with O(log n)
+
+	// to calculate the sum of the subarray [i; j] (inclusive), the formula is ps[j + 1] - ps[i]
+
+	minSubarraySize := len(nums) + 1
+
+	for i := 0; i < n; i++ {
+		// for every a[i], we search the smallest a[j] so that
+		// prefixSums[j + 1] - prefixSums[i] >= target
+
+		// this is a binary search starting from i
+		// !!! Note that we return j that is in [i; n] range, not the prefixSum range
+		minJ := binarySearch(prefixSums, i, target)
+
+		// if j >= n then insert position is after the array -> j is not found
+		if minJ < n { // j = n - 1 -> prefixSums[n] is still valid
+			subarraySize := minJ - i + 1
+			minSubarraySize = min(minSubarraySize, subarraySize)
+		}
+	}
+
+	// no matching subarray found -> return 0
+	if minSubarraySize > len(nums) {
+		return 0
+	}
+
+	return minSubarraySize
+}
+
+func binarySearch(ps []int, leftStart int, target int) int {
+	left := leftStart
+
+	// but since we're using ps[mid + 1], this is set to (N + 1), NOT (N + 2)
+	// I.e. in case of insertion after the array, we will return N + 1 that is out of bounds for the original array of size N
+	right := len(ps) - 1 // insertion point can be after the array, len is (N + 1)
+
+	for left < right {
+		mid := (left + right) / 2
+
+		// !!! Notice mid + 1, so that we'll return j, NOT j + 1
+		if ps[mid+1]-ps[leftStart] >= target { // target condition
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	// returned is the leftmost value where condition is satisfied
+	return left
 }
 
 func test(arr []int, target int, expectedResult int) { // nodes can be null
@@ -79,7 +151,7 @@ func test3() {
 
 func main() {
 	// 209. Minimum Size Subarray Sum
-	test1()
-	test2()
+	//test1()
+	//test2()
 	test3()
 }
