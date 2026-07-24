f8214eb18ac118481913d9ddeb25f55460ffaa13
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-24 22:39:34 +0200

    feat: Working O(n) solutions for "523. Continuous Subarray Sum". Use just the K-remainders of the prefix sums, no need for further iteration. - Keep just the "remainder to first position" map for the prefixSums, if remainder matches with the existing reminder and distance between them >= 2, then it's a solution.

--- continuousSubarraySum/continuousSubarraySum.go
+++ continuousSubarraySum/continuousSubarraySum.go
@@ -5,7 +5,75 @@ import (
 )
 
 func checkSubarraySum(nums []int, k int) bool {
-	// this is O(n^2) solution
+	return checkSubarraySum_optimized_2(nums, k) // O(n), O(k) space, just on counting the prefixSum values, storing just the "remainder to first position" map
+	//return checkSubarraySum_optimized(nums, k) // O(n), O(n) + O(k) space just on counting the prefixSum values
+	//return checkSubarraySum_naive(nums, k) // O(n^2), fails on TLE
+}
+
+func checkSubarraySum_optimized_2(nums []int, k int) bool {
+	// this is an O(n) solution, just filling in the prefix sums
+	// this is space-optimized to O(k) just for the map -> we don't need to keep the prefixSums array, we're just keeping the remainders
+
+	// todo: for the faster/less space in Go, we can use an int[] of size k for all the remainder in [0; k - 1] range
+	// map of "remainder by K" to "first prefixSum position with this remainder"
+	m := make(map[int]int)
+
+	// let's make correct prefix sums, with 0 and index = i + 1
+	// !!! we don't need the prefixSums array, just the previous and the current sum
+	currentSum := 0
+	m[0] = 0
+
+	for i, v := range nums {
+		currentSum = currentSum + v
+
+		remainder := currentSum % k
+
+		if remainderFirstIndex, ok := m[remainder]; ok { // remainder already exists
+			if ((i + 1) - remainderFirstIndex) >= 2 {
+				return true
+			}
+		} else { // remainder found the first time -> put the first index into the map
+			m[remainder] = i + 1
+		}
+	}
+
+	return false
+}
+
+func checkSubarraySum_optimized(nums []int, k int) bool {
+	// this is an O(n) solution, just filling in the prefix sums
+
+	n := len(nums)
+	prefixSums := make([]int, n+1)
+
+	// map of "remainder by K" to "first prefixSum position with this remainder"
+	m := make(map[int]int)
+
+	// let's make correct prefix sums, with 0 and index = i + 1
+	prefixSums[0] = 0
+	m[0] = 0
+
+	for i, v := range nums {
+		prefixSums[i+1] = prefixSums[i] + v
+
+		remainder := prefixSums[i+1] % k
+
+		if remainderFirstIndex, ok := m[remainder]; ok { // remainder already exists
+			if ((i + 1) - remainderFirstIndex) >= 2 {
+				return true
+			}
+		} else { // remainder found the first time -> put the first index into the map
+			m[remainder] = i + 1
+		}
+	}
+
+	fmt.Printf("Prefix sums: %v \n", prefixSums)
+
+	return false
+}
+
+func checkSubarraySum_naive(nums []int, k int) bool {
+	// this is an O(n^2) solution
 	// it gives TLE on big test-case 95
 
 	n := len(nums)
@@ -83,10 +151,19 @@ func test4() {
 	test(arr, k, expected)
 }
 
+func test5() {
+	arr := []int{5, 0, 0, 0}
+	k := 3
+	expected := true
+
+	test(arr, k, expected)
+}
+
 func main() {
 	// 523. Continuous Subarray Sum
 	test1()
 	test2()
 	test3()
 	test4()
+	test5()
 }
