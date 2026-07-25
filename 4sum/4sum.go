ed2161c507d0a6c62f625cfc36c2e946d132274d
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-25 15:29:04 +0200

    feat: Working solution of "18. 4Sum", this is just an extension of solution copy-pasted from the current "15. 3Sum" solution.

---
+++ 4sum/4sum.go
@@ -0,0 +1,67 @@
+package main
+
+import "slices"
+
+func fourSum(nums []int, target int) [][]int {
+	slices.Sort(nums)
+
+	// result are the values, not indexes
+	result := make([][]int, 0)
+
+	for i := 0; i < len(nums)-3; i++ { // 3 remaining values at the end are required for search values 2, 3, 4
+		if (i > 0) && (nums[i] == nums[i-1]) {
+			// skip duplicate a[i] values
+			continue
+		}
+
+		for j := i + 1; j < len(nums)-2; j++ { // 2 remaining values at the end are required for search values 3, 4
+			if (j > i+1) && (nums[j] == nums[j-1]) {
+				// skip duplicate a[j] values
+				continue
+			}
+
+			// search with twoSum O(n) on the remaining array
+			twoSum(nums, i, j, target, &result) // will append to result
+		}
+	}
+
+	return result
+}
+
+func twoSum(arr []int, firstElementIndex int, secondElementIndex int, target int, result *[][]int) {
+	firstElement := arr[firstElementIndex]
+	secondElement := arr[secondElementIndex]
+
+	left := secondElementIndex + 1
+	right := len(arr) - 1
+
+	for left < right {
+		sum := firstElement + secondElement + arr[left] + arr[right]
+
+		if sum == target { // found the result!
+			*result = append(*result, []int{firstElement, secondElement, arr[left], arr[right]})
+
+			// skip duplicates from left
+			for left < right && arr[left] == arr[left+1] {
+				left++
+			}
+
+			// skip duplicates from right
+			for left < right && arr[right] == arr[right-1] {
+				right--
+			}
+		}
+
+		if sum < target {
+			// we need to increase -> increase the smaller
+			left++
+		} else {
+			// we need to decrease -> decrease the bigger
+			right--
+		}
+	}
+}
+
+func main() {
+	// 18. 4Sum
+}
