c2729e0d4a781afed96463c1468df3385436973a
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-27 00:24:58 +0200

    feat: Solution of "724. Find Pivot Index". Straightforward - O(n) space, and optimized - O(1) space.

--- findPivotIndex/findPivotIndex.go
+++ findPivotIndex/findPivotIndex.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func pivotIndex(nums []int) int {
+	return pivotIndexOptimized(nums)
+	//return pivotIndexNaive(nums)
+}
+
+func pivotIndexOptimized(nums []int) int {
+	// not keeping the whole prefixSums array
+	// ps[n] is just a totalSum or the whole array
+	// ps[0] is 0
+	// leftSum we can calculate dynamically
+
+	totalSum := 0
+
+	for _, v := range nums {
+		totalSum += v
+	}
+
+	leftSum := 0
+
+	for i, v := range nums {
+		// left sum is now a sum of a[0] to a[i - 1], i.e. it is ps[i]
+		// totalSum = sum(a[0] - a[i - 1]) + a[i] + sum(a[i + 1] to a[n - 1])
+		// totalSum = leftSum + a[i] + rightSum
+		// -> rightSum = totalSum - leftSum - a[i]
+		rightSum := totalSum - leftSum - v
+
+		if leftSum == rightSum {
+			return i
+		}
+
+		// we only increase for the next index, i.e. we have ps[i], i.e. for the previous element
+		leftSum += v
+	}
+
+	return -1
+}
+
+func pivotIndexNaive(nums []int) int {
 	n := len(nums)
 	ps := make([]int, n+1)
 
@@ -19,6 +58,7 @@ func pivotIndex(nums []int) int {
 
 		// sum from a[0] to a[i - 1] -> ps[i] - ps[0]
 		// left sum = ps[i] - ps[0]
+		// ps[0] is actually 0
 		leftSum := ps[i] - ps[0]
 
 		// sum from a[i + 1] to a[n - 1] -> ps[n] - ps[i + 1]
@@ -45,5 +85,4 @@ func main() {
 	result := pivotIndex(arr)
 	fmt.Printf("Result: %v \n", result)
 	fmt.Printf("Expected result: %v \n", expected)
-
 }
