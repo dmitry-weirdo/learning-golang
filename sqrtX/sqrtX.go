ecaf9aec318cace13428bf840df2f0e13b2dfe84
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-07-28 23:37:19 +0200

    feat: Solution of "69. Sqrt(x)" with a targeted binary search: `while (left <= right)` and `return right` (if exact target not found).

--- sqrtX/sqrtX.go
+++ sqrtX/sqrtX.go
@@ -5,7 +5,8 @@ import (
 )
 
 func mySqrt(x int) int {
-	return mySqrt_overflowSafe(x)
+	return mySqrt_targetBinarySearch(x)
+	//return mySqrt_overflowSafe(x)
 	//return mySqrt_naive(x)
 }
 
@@ -32,6 +33,33 @@ func mySqrt_overflowSafe(x int) int {
 	}
 
 	return left
+}
+
+func mySqrt_targetBinarySearch(x int) int {
+	if x < 2 {
+		return x
+	}
+
+	left := 1      // 0 was checked above
+	right := x / 2 // square is <= than x / 2 - wrong for 0 and 1, but we exited before
+
+	//result := -1
+
+	for left <= right { // <=
+		mid := left + (right-left)/2 // overflow-safe
+
+		if mid > x/mid { // too big -> not a result
+			right = mid - 1
+		} else if mid < x/mid { // less than x -> matches, let's save it
+			left = mid + 1
+			//result = mid
+		} else {
+			return mid
+		}
+	}
+
+	// !!! for this option of binary search and for what we search for
+	return right
 }
 
 func mySqrt_naive(x int) int {
@@ -64,7 +92,7 @@ func test(x int, expectedResult int) {
 
 	fmt.Printf("X: %v \n", x)
 
-	result := mySqrt_overflowSafe(x)
+	result := mySqrt(x)
 
 	fmt.Printf("Rounded-down sqrt of %v: %v \n", x, result)
 	fmt.Printf("Expected rounded-down sqrt of %v: %v \n", x, expectedResult)
@@ -102,10 +130,18 @@ func test4() {
 	test(x, expected)
 }
 
+func test5() {
+	x := 101
+	expected := 10
+
+	test(x, expected)
+}
+
 func main() {
 	// 69. Sqrt(x)
 	test1()
 	test2()
 	test3()
 	test4()
+	test5()
 }
