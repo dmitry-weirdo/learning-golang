e475d4bfa82fd99ae22711d3b3e250a9be65edd4
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-08-20 19:17:40 +0200

    feat: Solution of "1971. Find if Path Exists in Graph" with union find the graph nodes is the fastest.

--- findIfPathExistsInGraph/findIfPathExistsInGraph.go
+++ findIfPathExistsInGraph/findIfPathExistsInGraph.go
@@ -1,19 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func validPath(n int, edges [][]int, source int, destination int) bool {
-	// todo: union-find should be the fastest!
+	// union-find should be the fastest!
+	// yes, it's very fast, passes in 12-25 ms
+	return validPath_unionFind(n, edges, source, destination)
 
 	// todo: we can do iterative DFS with fast return
 
 	// BFS is much faster
 	// passes in 100-110 ms
-	return validPath_bfs(n, edges, source, destination)
+	//return validPath_bfs(n, edges, source, destination)
 
 	// trivial DFS, no backtracking, marking visited and moving to visited again
 	// passes in 215-280 ms
 	//return validPath_dfs_recursive(n, edges, source, destination)
+}
+
+func validPath_unionFind(n int, edges [][]int, source int, destination int) bool {
+	uf := newUnionFind(n)
+
+	for _, v := range edges {
+		uf.union(v[0], v[1])
+	}
+
+	return uf.find(source) == uf.find(destination)
 }
 
 func validPath_bfs(n int, edges [][]int, source int, destination int) bool {
@@ -133,6 +147,75 @@ func createAdjacencyListUndirected(n int, edges [][]int) [][]int {
 	}
 
 	return adj
+}
+
+type UnionFind struct {
+	parents []int // if parent[i] = i, it is the root, else it's the index of the parent
+	sizes   []int // sizes of the tree for every element
+}
+
+func newUnionFind(n int) UnionFind {
+	parents := make([]int, n)
+	sizes := make([]int, n)
+
+	for i := range n {
+		// every group is just a root
+		parents[i] = i
+
+		// every group has a size of 1
+		sizes[i] = 1
+	}
+
+	return UnionFind{
+		parents: parents,
+		sizes:   sizes,
+	}
+}
+
+func (uf UnionFind) find(x int) int { // recursive version
+	if uf.parents[x] == x { // parent points to itself -> reached the root
+		return x
+	}
+
+	// path compression -> set the root to every parents[i] in the chain
+	uf.parents[x] = uf.find(uf.parents[x])
+
+	return uf.parents[x]
+}
+
+func (uf UnionFind) print() {
+	fmt.Printf("Parents: %v \n", uf.parents)
+	fmt.Printf("Sizes: %v \n", uf.sizes)
+}
+
+func (uf UnionFind) union(x, y int) bool { // returns false if they're already in the same set
+	// these find will perform path compression
+	rootX := uf.find(x)
+	rootY := uf.find(y)
+
+	//fmt.Printf("root of %d: %d, root of %d: %d\n", x, rootX, y, rootY)
+
+	// x and y are already in the same set -> nothing to merge
+	if rootX == rootY {
+		//fmt.Printf("Element %v and %v already belong to the same root %v. Nothing to merge. \n", x, y, rootX)
+		return false
+	}
+
+	// merge the smaller group into the bigger group
+	// todo: ideally, we should merge the tree with smaller depth into the tree with bigger depth
+	if uf.sizes[rootX] < uf.sizes[rootY] { // merge x into y
+		//fmt.Printf("sizes[%v] = %v < sizes[%v] = %v. Merging root %v into root %v \n", rootX, uf.sizes[rootX], rootY, uf.sizes[rootY], rootX, rootY)
+
+		uf.parents[rootX] = rootY
+		uf.sizes[rootY] += uf.sizes[rootX]
+	} else { // merge y into x
+		//fmt.Printf("sizes[%v] = %v >= sizes[%v] = %v. Merging root %v into root %v \n", rootX, uf.sizes[rootX], rootY, uf.sizes[rootY], rootY, rootX)
+
+		uf.parents[rootY] = rootX
+		uf.sizes[rootX] += uf.sizes[rootY]
+	}
+
+	return true
 }
 
 func test(n int, edges [][]int, source int, destination int, expectedResult bool) {
