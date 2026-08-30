e17424a0707b9a3b7807a0deee6b22090004932c
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-08-30 18:27:52 +0200

    test: Add normal format tests to "04. Maximum Depth of Binary Tree".

--- binaryTreeMaxDepth/binaryTreeMaxDepth.go
+++ binaryTreeMaxDepth/binaryTreeMaxDepth.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
-
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+import (
+	"demo/trees"
+	. "demo/trees" // not recommended, but ok for LeetCode -> to use TreeNode without prefix
+	"fmt"
+)
 
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 
-	fmt.Printf("Val: %v \n", root.Val)
+	//fmt.Printf("Val: %v \n", root.Val)
 
 	maxDepthLeft := maxDepth(root.Left)
 	maxDepthRight := maxDepth(root.Right)
@@ -21,20 +19,41 @@ func maxDepth(root *TreeNode) int {
 	return max(maxDepthLeft, maxDepthRight) + 1
 }
 
+func test(arr []any, expectedResult int) { // nodes can be nil
+	fmt.Println()
+	fmt.Println("====================")
+
+	fmt.Printf("Tree array: %v \n", arr)
+
+	tree := trees.TreeFromArray(arr)
+	fmt.Printf("Tree: \n")
+	trees.PrintTreeTopDown(tree)
+
+	result := maxDepth(tree)
+
+	fmt.Printf("Max tree depth: %v \n", result)
+	fmt.Printf("Expected result: %v \n", expectedResult)
+
+	if result != expectedResult {
+		fmt.Printf("FAILURE: expected result = %v, actual result = %v \n", expectedResult, result)
+	}
+}
+
+func test1() {
+	test([]any{3, 9, 20, nil, nil, 15, 7}, 3) // 3 - 20 - 15, 3- 20 - 7
+}
+
+func test2() {
+	test([]any{1, nil, 2}, 2) // 1 - 2
+}
+
+func test3() {
+	test([]any{}, 0) // no nodes in the tree
+}
+
 func main() {
 	// 04. Maximum Depth of Binary Tree
-	r11 := &TreeNode{15, nil, nil}
-	r12 := &TreeNode{7, nil, nil}
-
-	r01 := &TreeNode{9, nil, nil}
-	r02 := &TreeNode{20, r11, r12}
-
-	root := &TreeNode{3, r01, r02}
-
-	result := maxDepth(root)
-
-	expectedResult := 3
-
-	fmt.Printf("Expected result: %v \n", expectedResult)
-	fmt.Printf("Result: %v \n", result)
+	test1()
+	test2()
+	test3()
 }
