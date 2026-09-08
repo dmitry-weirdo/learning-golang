109dfde468494edb8d0ff9508d63a43122f9b37d
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-09-08 14:45:32 +0200

    test: Add test-cases to "701. Insert into a Binary Search Tree".

--- insertIntoABinarySearchTree/insertIntoABinarySearchTree.go
+++ insertIntoABinarySearchTree/insertIntoABinarySearchTree.go
@@ -1,10 +1,10 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+import (
+	"demo/trees"
+	. "demo/trees" // not recommended, but ok for LeetCode -> to use TreeNode without package prefix
+	"fmt"
+)
 
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	//return insertIterative(root, val)
@@ -64,6 +64,67 @@ func insertIterative(root *TreeNode, val int) *TreeNode {
 	return root
 }
 
+func test(arr []any, val int, expectedResult []any) { // nodes can be null
+	fmt.Println()
+	fmt.Println("====================")
+
+	fmt.Printf("Tree array: %v \n", arr)
+
+	tree := trees.TreeFromArray(arr)
+	fmt.Printf("Initial tree: \n")
+	trees.PrintTreeTopDown(tree)
+
+	fmt.Printf("Value to insert: %v \n", val)
+
+	result := insertIntoBST(tree, val)
+
+	fmt.Printf("Tree with inserted node %v: \n", val)
+	trees.PrintTreeTopDown(result)
+
+	resultAsArray := trees.TreeToArray(result)
+	fmt.Printf("Result tree as array: %v \n", resultAsArray)
+	fmt.Printf("Expected result:      %v \n", expectedResult)
+
+	if len(resultAsArray) != len(expectedResult) {
+		fmt.Printf("FAILURE: expected result length = %v, actual result length = %v \n", len(expectedResult), len(resultAsArray))
+		return
+	}
+
+	for i, v := range resultAsArray {
+		if v != expectedResult[i] {
+			fmt.Printf("FAILURE: expected result[%v] = %v, actual result[%v] = %v \n", i, expectedResult[i], i, v)
+			return
+		}
+	}
+}
+
+func test1() {
+	test(
+		[]any{4, 2, 7, 1, 3, nil, nil},
+		5,
+		[]any{4, 2, 7, 1, 3, 5, nil},
+	)
+}
+
+func test2() {
+	test(
+		[]any{40, 20, 60, 10, 30, 50, 70},
+		25,
+		[]any{40, 20, 60, 10, 30, 50, 70, nil, nil, 25, nil, nil, nil, nil, nil},
+	)
+}
+
+func test3() {
+	test(
+		[]any{4, 2, 7, 1, 3, nil, nil},
+		5,
+		[]any{4, 2, 7, 1, 3, 5, nil},
+	)
+}
+
 func main() {
 	// 701. Insert into a Binary Search Tree
+	test1()
+	test2()
+	test3()
 }
