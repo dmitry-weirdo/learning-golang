2c98a8a02477712365f69ec7b742b3b3c6171074
Dmitriy Popov <dmitry.weirdo@gmail.com>
2026-09-26 00:59:26 +0200

    feat: Solution of "1693. Daily Leads and Partners". Basic stuff, teaches `count(distinct ...)` queries.

---
+++ dailyLeadsAndPartners/dailyLeadsAndPartners.sql
@@ -0,0 +1,8 @@
+-- 1693. Daily Leads and Partners
+select
+ds.date_id,
+ds.make_name,
+count(distinct ds.lead_id) as "unique_leads",
+count(distinct ds.partner_id) as "unique_partners"
+from DailySales ds
+group by ds.date_id, ds.make_name
